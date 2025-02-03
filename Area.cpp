796eade77b2048595e574a71688113e9ba526605
Wabz-a11y <ianwabz102@gmail.com>
2025-02-03 11:45:31 +0300

    initial commit

---
+++ Area.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+#define PI 3.142
+
+double Area(double radius){
+       return radius*radius*PI;
+}
+int main (){
+    int Area;
+    int radius;
+
+    cout<<"Enter radius. \n"<<radius;
+    cin>>radius;
+        
+    double CircleArea(double Area);
+    cout<<"Area is: \n"<<CircleArea<<endl;
+
+return 0;
+}
+
